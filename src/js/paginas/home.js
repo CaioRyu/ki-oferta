e3d9f20dda4dd73c10671959e5b3b678872e5aa9
CaioRyu <caioaugustoryu@gmail.com>
2026-08-27 22:03:11 -0300

    criação de estrutura inicial do projeto

---
+++ src/js/paginas/home.js
@@ -0,0 +1,16 @@
+function home(app){
+    app.innerHTML = `
+    <div>
+        <h1>Página Inicial </h1>
+        <div class="titulodiv">
+          
+        </div>
+    </div>
+    `
+}
+
+export default {
+    url : "#home",
+    label : "inicio",
+    pagina : home
+}
---
+++ src/js/paginas/sobre.js
@@ -0,0 +1,13 @@
+function sobre(app){
+    app.innerHTML = `
+    <div>
+        <h1>Página Inicial </h1>
+    </div>
+    `
+}
+
+export default {
+    url : "#home",
+    label : "sobre",
+    pagina : sobre
+}
