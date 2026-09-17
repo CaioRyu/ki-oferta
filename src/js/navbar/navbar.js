4140423d601c2565978c506391a9dc2456f99055
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-09-17 16:33:07 -0300

    Arquivos js atualizdos com Bootstrap

--- src/js/navbar/navbar.js
+++ src/js/navbar/navbar.js
@@ -1,15 +1,23 @@
-function navbar(item_menu){
-const navbar = document.getElementById('navbar');
-navbar.innerHTML = `
-<nav class="navbar">
-        ${
-            item_menu.filter(menu=> menu.label !== "")
-            .map((item)=>{
-                return `<li><a href="${item.url}" class="navbar-item">${item.label}</a></li>`
-            }).join('')
-        }
-</nav>`;
+function navbar(item_menu) {
+  const navbar = document.getElementById('navbar');
 
+  const itens = item_menu
+    .filter((menu) => menu.label !== "")
+    .map(
+      (item) =>
+        `<li class="nav-item">
+           <a href="${item.url}" class="navbar-item nav-link">${item.label}</a>
+         </li>`
+    )
+    .join("");
+
+  navbar.innerHTML = `
+    <nav class="navbar-bottom d-flex justify-content-center align-items-center gap-1">
+      <ul class="nav list-unstyled d-flex gap-1 m-0 p-0">
+        ${itens}
+      </ul>
+    </nav>
+  `;
 }
 
 export { navbar };
