41e6e934a8859dc0f3aa1cafea9b86faa2b78cf5
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-09-18 18:27:10 -0300

    fix: Navbar

--- src/js/navbar/navbar.js
+++ src/js/navbar/navbar.js
@@ -1,23 +1,17 @@
 function navbar(item_menu) {
   const navbar = document.getElementById('navbar');
 
-  const itens = item_menu
-    .filter((menu) => menu.label !== "")
-    .map(
-      (item) =>
-        `<li class="nav-item">
+  navbar.innerHTML = `
+    <nav class="navbar">
+    ${
+      item_menu.filter(menu => menu.label !== "")
+      .map((item) =>{
+        return `<li> 
            <a href="${item.url}" class="navbar-item">${item.label}</a>
          </li>`
-    )
-    .join("");
-
-  navbar.innerHTML = `
-    <nav class="navbar-bottom">
-      <ul class="nav">
-        ${itens}
-      </ul>
-    </nav>
-  `;
+      }).join('')
+    }
+    </nav>`;
 }
 
 export { navbar };
