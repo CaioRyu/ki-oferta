c99d494d7d45096b8cf9b8f2e6220fb1172993c5
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-09-24 16:18:17 -0300

    refactor: Itens dos Produtos

--- src/js/dadosMockados/produtos.js
+++ src/js/dadosMockados/produtos.js
@@ -23,13 +23,24 @@ function produtos(app) {
                         placeholder="Digite um produto..."
                         aria-label="Buscar produto"
                     />
-                    <button id="btn-busca" class="btn" type="submit">Buscar</button>
+                    <button id="btn-busca-produtos" class="btn-produtos" type="submit">Buscar</button>
                 </form>
             </header>
             ${
                listaDeProdutos.map(item=> `
-                <div class="item-produtos"> 
-                    Nome: ${item.nome} Preço:${item.preco } Distância:${item.distancia}
+                <div class="item-produtos">
+                    <div class="descricao-produto">
+                        <div class="imagem-produto">
+                            <img class="imagem-produto" src="./assets/icon/favicon.ico" />
+                        </div>
+                        <div class="detalhes-produto">
+                        <p class="nome-produto"> ${item.nome} </p>
+                        <p class="distancia-produto"> Distância: ${item.distancia} m </p>
+                        </div>
+                    </div>
+                    <div class="preco-produto-div">
+                        <p class="preco-produto">R$${item.preco.toFixed(2)} </p>
+                    </div>
                 </div> ` ).join('<br>')
             }
         </div>
